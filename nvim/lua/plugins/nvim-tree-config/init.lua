72e159c894bbb78d40dc763e0740be0a63e7557f
charlie <astromech12000@gmail.com>
2022-09-05 11:04:53 +0900

    Updated nvim-tree config.

--- nvim/lua/plugins/nvim-tree-config/init.lua
+++ nvim/lua/plugins/nvim-tree-config/init.lua
@@ -1,7 +1,6 @@
 vim.api.nvim_set_keymap('', "<C-\\>", ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 require('nvim-tree').setup {
-  open_on_tab = true,
   update_focused_file = {
     enable = true,
     update_cwd = false,
