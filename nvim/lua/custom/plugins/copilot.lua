ac507c3e8405be0d4ddc2b4ff82dcafc40a22e76
charlie <astromech12000@gmail.com>
2023-08-17 04:17:50 +0900

    update copilot

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -1,25 +1,27 @@
 return {
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   cmd = 'Copilot',
-  --   event = 'InsertEnter',
-  --   config = function ()
-  --     require('copilot').setup({
-  --       suggestion = { enabled = false },
-  --       panel = { enabled = false },
-  --     })
-  --   end
-  -- },
-  -- {
-  --   'zbirenbaum/copilot-cmp',
-  --   dependencies = { 'zbirenbaum/copilot.lua' },
-  --   event = 'InsertEnter',
-  --   config = function ()
-  --     require('copilot_cmp').setup()
-  --   end
-  -- },
   {
-    'hrsh7th/cmp-copilot',
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
     event = 'InsertEnter',
-  }
+    config = function ()
+      require('copilot').setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    dependencies = { 'zbirenbaum/copilot.lua' },
+    config = function ()
+      require('copilot_cmp').setup()
+    end
+  },
+  -- {
+  --   'hrsh7th/cmp-copilot',
+  --   event = 'InsertEnter',
+  -- },
+  -- {
+  --   'github/copilot.vim',
+  -- },
 }
