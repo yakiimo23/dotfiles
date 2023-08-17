2e40eb2e271f341ed098931c059896f52ac3a6dc
charlie <astromech12000@gmail.com>
2023-08-18 00:25:53 +0900

    feat(tree-sitter): add ruby language to ensure_installed list

--- nvim/lua/custom/plugins/tree-sitter.lua
+++ nvim/lua/custom/plugins/tree-sitter.lua
@@ -7,7 +7,7 @@ return {
     pcall(require('nvim-treesitter.install').update { with_sync = true })
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'ruby' },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = false,
