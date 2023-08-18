340833e36cf235317067508a54cfbf928a5c4f62
charlie <astromech12000@gmail.com>
2023-08-19 02:30:39 +0900

    refactor(lsp): remove Rubocop LSP configuration

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -86,16 +86,6 @@ return {
     }
 
     -- Rubocop
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "ruby",
-      callback = function()
-        vim.lsp.start {
-          name = "rubocop",
-          cmd = { "bundle", "exec", "rubocop", "--lsp" },
-        }
-      end,
-    })
-
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "ruby",
       callback = function()
