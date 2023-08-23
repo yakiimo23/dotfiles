8b97db479bbf1580c71848f00f160b46a7adefe2
charlie <astromech12000@gmail.com>
2023-08-23 16:53:41 +0900

    style: Add filename to lualine_c sections

--- nvim/init.lua
+++ nvim/init.lua
@@ -55,6 +55,11 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          { 'filename', path = 1 }
+        }
+      }
     },
   },
 
