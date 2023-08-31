dd036dced6ae21d526090020e0baf5eb86f70f95
charlie <astromech12000@gmail.com>
2023-09-01 00:22:24 +0900

    style: disable indent option

--- nvim/lua/custom/plugins/tree-sitter.lua
+++ nvim/lua/custom/plugins/tree-sitter.lua
@@ -13,7 +13,7 @@ return {
       auto_install = false,
 
       highlight = { enable = true },
-      indent = { enable = true, disable = { 'python' } },
+      indent = { enable = false, disable = { 'python' } },
       incremental_selection = {
         enable = true,
         keymaps = {
