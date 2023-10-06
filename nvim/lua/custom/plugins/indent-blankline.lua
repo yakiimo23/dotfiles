5ea3bd526c4f22bde6a0842b87a1539b431dbc8e
charlie <astromech12000@gmail.com>
2023-10-06 13:55:08 +0900

    refactor: Remove redundant code and simplify configuration for indent-blankline plugin

--- nvim/lua/custom/plugins/indent-blankline.lua
+++ nvim/lua/custom/plugins/indent-blankline.lua
@@ -1,23 +1,14 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  -- dependencies = { 'TheGLander/indent-rainbowline.nvim' },
+  -- opt = function(_, opts)
+  --   return require("indent-rainbowline").make_opts(opts)
+  -- end,
   config = function()
-    vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
-    require("indent_blankline").setup {
-      space_char_blankline = " ",
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-    }
+    require('ibl').setup({
+      indent = { char = '│', },
+      scope = { enabled = true },
+    })
   end,
 }
