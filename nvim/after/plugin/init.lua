c0376442d54963bf064f139843152aa4ceb86025
charlie <astromech12000@gmail.com>
2024-02-05 01:51:47 +0900

    ✨ feat(nvim): add trouble.nvim plugin and keybindings for better debugging experience 📦 new(nvim): add new file trouble.lua to manage trouble.nvim plugin configuration

--- nvim/after/plugin/init.lua
+++ nvim/after/plugin/init.lua
@@ -50,3 +50,11 @@ end, { desc = 'Run Rspec Nearest in toggleterm', silent = true })
 
 -- Bug fix for tree-sitter
 vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
+
+-- trouble
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, { desc = 'Toggle Trouble' })
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = 'Toggle Workspace Diagnostics' })
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end, { desc = 'Toggle Document Diagnostics' })
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, { desc = 'Toggle Quickfix' })
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, { desc = 'Toggle Loclist' })
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = 'Toggle Lsp References' })
