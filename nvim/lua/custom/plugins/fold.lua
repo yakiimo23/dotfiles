17bba3413c442436e30ea9f9d6694c32780f20be
charlie <astromech12000@gmail.com>
2024-02-07 03:07:48 +0900

    🔧 refactor(fold.lua): simplify fold_virt_text_handler and remove unnecessary calculations 🐛 fix(fold.lua): uncomment winhighlight property to restore highlighting functionality 🎨 style(fold.lua): improve code readability by reformatting table insertions and string repetitions

--- nvim/lua/custom/plugins/fold.lua
+++ nvim/lua/custom/plugins/fold.lua
@@ -12,7 +12,7 @@ return {
     preview = {
       win_config = {
         border = { "", "─", "", "", "", "─", "", "" },
-        -- winhighlight = "Normal:Folded",
+        winhighlight = "Normal:Folded",
         winblend = 0,
       },
       mappings = {
@@ -33,9 +33,7 @@ return {
   config = function(_, opts)
     local handler = function(virtText, lnum, endLnum, width, truncate)
       local newVirtText = {}
-      local totalLines = vim.api.nvim_buf_line_count(0)
-      local foldedLines = endLnum - lnum
-      local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+      local suffix = (' 󰁂 %d '):format(endLnum - lnum)
       local sufWidth = vim.fn.strdisplaywidth(suffix)
       local targetWidth = width - sufWidth
       local curWidth = 0
@@ -47,24 +45,24 @@ return {
         else
           chunkText = truncate(chunkText, targetWidth - curWidth)
           local hlGroup = chunk[2]
-          table.insert(newVirtText, { chunkText, hlGroup })
+          table.insert(newVirtText, {chunkText, hlGroup})
           chunkWidth = vim.fn.strdisplaywidth(chunkText)
           -- str width returned from truncate() may less than 2nd argument, need padding
           if curWidth + chunkWidth < targetWidth then
-            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
           end
           break
         end
         curWidth = curWidth + chunkWidth
       end
-      local rAlignAppndx =
-        math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
-      suffix = (" "):rep(rAlignAppndx) .. suffix
-      table.insert(newVirtText, { suffix, "MoreMsg" })
+      table.insert(newVirtText, {suffix, 'MoreMsg'})
       return newVirtText
     end
-    opts["fold_virt_text_handler"] = handler
-    require("ufo").setup(opts)
+
+    require('ufo').setup({
+      fold_virt_text_handler = handler
+    })
+
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
     vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
