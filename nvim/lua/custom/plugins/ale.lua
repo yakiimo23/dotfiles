b5e2f6f61c18d193cd9bc3c30fb3bd38f3e692ab
charlie <astromech12000@gmail.com>
2024-02-08 04:00:44 +0900

    🔧 refactor(ale.lua): comment out dense-analysis/ale configuration to disable it 🔧 refactor(null-ls.lua): replace jose-elias-alvarez/null-ls.nvim with nvimtools/none-ls.nvim to simplify configuration

--- nvim/lua/custom/plugins/ale.lua
+++ nvim/lua/custom/plugins/ale.lua
@@ -2,56 +2,58 @@ if vim.g.vscode then
   return {}
 end
 
-return {
-  'dense-analysis/ale',
-  config = function()
-    vim.g.ale_linters = {
-      -- ['*'] = { 'aspell', 'hunspell' },
-      ['*'] = { 'cspell' },
-    }
+return {}
 
-    vim.g.ale_cspell_options = '--config /Users/charlie/.config/nvim/spell/cspell.json'
-
-    local function add_word_to_cspell()
-      local word = vim.fn.expand('<cword>')
-      local cspell_file = '/Users/charlie/.config/nvim/spell/cspell.json'  -- cspell.json ファイルのパス
-
-      -- JSON ファイルを読み込む
-      local file, err = io.open(cspell_file, 'r')
-      if not file then
-        print('Failed to open ' .. cspell_file .. ': ' .. err)
-        return
-      end
-
-      local data = file:read('*a')
-      file:close()
-
-      -- JSON データを解析する
-      local status, json = pcall(vim.fn.json_decode, data)
-      if not status then
-        print('Failed to parse ' .. cspell_file .. ': ' .. json)
-        return
-      end
-
-      -- 単語を追加する
-      table.insert(json.words, word)
-
-      -- JSON ファイルを更新する
-      file, err = io.open(cspell_file, 'w')
-      if not file then
-        print('Failed to open ' .. cspell_file .. ': ' .. err)
-        return
-      end
-
-      file:write(vim.fn.json_encode(json))
-      file:close()
-
-      print('Added "' .. word .. '" to ' .. cspell_file)
-
-      -- ALE を再実行する
-      vim.cmd('ALELint')
-    end
-
-    vim.api.nvim_create_user_command('AddWordToCSpell', add_word_to_cspell, {})
-  end,
-}
+-- return {
+--   'dense-analysis/ale',
+--   config = function()
+--     vim.g.ale_linters = {
+--       -- ['*'] = { 'aspell', 'hunspell' },
+--       ['*'] = { 'cspell' },
+--     }
+--
+--     vim.g.ale_cspell_options = '--config /Users/charlie/.config/nvim/spell/cspell.json'
+--
+--     local function add_word_to_cspell()
+--       local word = vim.fn.expand('<cword>')
+--       local cspell_file = '/Users/charlie/.config/nvim/spell/cspell.json'  -- cspell.json ファイルのパス
+--
+--       -- JSON ファイルを読み込む
+--       local file, err = io.open(cspell_file, 'r')
+--       if not file then
+--         print('Failed to open ' .. cspell_file .. ': ' .. err)
+--         return
+--       end
+--
+--       local data = file:read('*a')
+--       file:close()
+--
+--       -- JSON データを解析する
+--       local status, json = pcall(vim.fn.json_decode, data)
+--       if not status then
+--         print('Failed to parse ' .. cspell_file .. ': ' .. json)
+--         return
+--       end
+--
+--       -- 単語を追加する
+--       table.insert(json.words, word)
+--
+--       -- JSON ファイルを更新する
+--       file, err = io.open(cspell_file, 'w')
+--       if not file then
+--         print('Failed to open ' .. cspell_file .. ': ' .. err)
+--         return
+--       end
+--
+--       file:write(vim.fn.json_encode(json))
+--       file:close()
+--
+--       print('Added "' .. word .. '" to ' .. cspell_file)
+--
+--       -- ALE を再実行する
+--       vim.cmd('ALELint')
+--     end
+--
+--     vim.api.nvim_create_user_command('AddWordToCSpell', add_word_to_cspell, {})
+--   end,
+-- }
--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -1,60 +1,17 @@
-return {}
+if vim.g.vscode then
+  return {}
+end
 
--- if vim.g.vscode then
---   return {}
--- end
--- return {
---   'jose-elias-alvarez/null-ls.nvim',
---   event = { 'BufReadPre', 'BufNewFile' },
---   dependencies = {
---     'davidmh/cspell.nvim',
---   },
---   config = function ()
---     local config = {
---       config_file_path = '~/.config/cspell/cspell.json',
---
---       find_json = function(cwd)
---       end,
---
---       on_success = function(cspell_config_file, params, action_name)
---         if action_name == 'add_to_json' then
---           os.execute(
---             string.format(
---               "cat %s | jq -S '.words |= sort' | tee %s > /dev/null",
---               cspell_config_file,
---               cspell_config_file
---             )
---           )
---         end
---       end
---     }
---     local conditional = function(fn)
---       local utils = require("null-ls.utils").make_conditional_utils()
---       return fn(utils)
---     end
---     local null_ls = require('null-ls')
---     local cspell = require('cspell')
---     null_ls.setup({
---       sources = {
---         cspell.diagnostics.with({ config = config }),
---         cspell.code_actions.with({ config = config }),
---         conditional(function(utils)
---           return utils.root_has_file('Gemfile')
---             and null_ls.builtins.diagnostics.rubocop.with({
---               command = 'bundle',
---               args = vim.list_extend({ 'exec', 'rubocop' }, null_ls.builtins.diagnostics.rubocop._opts.args),
---             })
---             or null_ls.builtins.diagnostics.rubocop
---         end),
---         conditional(function(utils)
---           return utils.root_has_file('Gemfile')
---             and null_ls.builtins.formatting.rubocop.with({
---               command = 'bundle',
---               args = vim.list_extend({ 'exec', 'rubocop' }, null_ls.builtins.formatting.rubocop._opts.args),
---             })
---             or null_ls.builtins.formatting.rubocop
---         end)
---       },
---     })
---   end
--- }
+return {
+  'nvimtools/none-ls.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function ()
+    local null_ls = require('null-ls')
+
+    null_ls.setup({
+      sources = {
+        -- null_ls.builtins.diagnostics.typos,
+      },
+    })
+  end
+}
