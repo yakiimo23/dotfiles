d3680ab338c75051b8f521691c035c5c818381bd
charlie <astromech12000@gmail.com>
2024-02-08 04:01:57 +0900

    ✨ feat(noice.lua): add new noice.nvim plugin configuration file for better UI/UX and LSP integration

---
+++ nvim/lua/custom/plugins/noice.lua
@@ -0,0 +1,32 @@
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+  config = function()
+    require('noice').setup({
+      routes = {
+        {
+          filter = { event = 'cmdline_show', kind = 'echomsg' },
+          opts = { skip = true },
+        }
+      },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = false,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = true,
+      },
+    })
+  end
+}
