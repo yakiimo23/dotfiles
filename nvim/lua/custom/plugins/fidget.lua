7744aa7e8bad24a24c5085fae0617dbbf7cb1a3c
charlie <astromech12000@gmail.com>
2024-02-09 15:26:34 +0900

    ✨ feat(nvim): add fidget.nvim plugin to custom plugins 🔥 remove(nvim): remove fidget.nvim from lsp.lua as it's now in custom plugins

---
+++ nvim/lua/custom/plugins/fidget.lua
@@ -0,0 +1,4 @@
+return {
+  'j-hui/fidget.nvim',
+  tag = 'v1.3.0',
+}
--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -5,10 +5,6 @@ return {
     { 'williamboman/mason.nvim', config = true },
     'williamboman/mason-lspconfig.nvim',
 
-    -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
-
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
   },
