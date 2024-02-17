f0fbefeb4051d8d78fc076c0ee6dca2664721fa3
charlie <astromech12000@gmail.com>
2024-02-18 01:49:29 +0900

    🔧 refactor(noice.lua): disable command_palette preset to improve performance and reduce unnecessary load

--- nvim/lua/custom/plugins/noice.lua
+++ nvim/lua/custom/plugins/noice.lua
@@ -22,7 +22,7 @@ return {
       },
       presets = {
         bottom_search = false,
-        command_palette = true,
+        command_palette = false,
         long_message_to_split = true,
         inc_rename = false,
         lsp_doc_border = true,
