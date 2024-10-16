3aa8d4f7746e84305856d64d62034be36382dae2
charo <masafumi.matsunaga@smarthr.co.jp>
2024-10-16 11:02:52 +0900

    add rubocop and solargraph config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,6 +7,20 @@ return {
           config = "~/.config/nvim/lua/plugins/.typos.toml",
         },
       },
+      rubocop = {
+        cmd = { "bundle", "exec", "rubocop", "--lsp" },
+      },
+      solargraph = {
+        cmd = { "solargraph", "stdio" },
+        settings = {
+          solargraph = {
+            diagnostics = true,
+            useBundler = false,
+            formatting = true,
+            reporters = { "require_not_found" },
+          },
+        },
+      },
     },
   },
 }
