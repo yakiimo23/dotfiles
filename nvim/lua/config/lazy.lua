71c0edd594ac4d86f1fecb3ae28da53ff34c8811
charo <masafumi.matsunaga@smarthr.co.jp>
2024-10-16 11:02:18 +0900

    disable go plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
