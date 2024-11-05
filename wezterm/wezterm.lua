03e3fd661be9e13ee4728c60fc61cce33e8b51be
charo <masafumi.matsunaga@smarthr.co.jp>
2024-11-05 14:39:35 +0900

    🔧 設定ファイル更新(wezterm.lua): 一貫性のためにシングルクォートをダブルクォートに変更

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,22 @@
-require 'format'
+require("format")
 
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
-local config = wezterm.config_builder();
+local config = wezterm.config_builder()
 
 -- color scheme
-config.color_scheme = 'tokyonight';
+config.color_scheme = "tokyonight"
 
 -- font
-config.font = wezterm.font('JetBrains Mono', { weight='Regular' });
-config.font_size = 18.0;
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+config.font_size = 18.0
 
 -- key bindings
-config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 2000 };
-config.keys = require('keybinds').keys;
-config.key_tables = require('keybinds').key_tables;
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
 
 -- status bar
-config.status_update_interval = 1000;
+config.status_update_interval = 1000
 
-return config;
+return config
