c454c02d2afc71987bafea37ff36c1fa9e2e72bc
charo <masafumi.matsunaga@smarthr.co.jp>
2025-03-18 17:43:11 +0900

    feat(lspconfig.lua): disable LSP configuration when running in VSCode to prevent conflicts refactor(lspconfig.lua): reformat code for better readability and maintainability

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,31 +1,35 @@
-return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      typos_lsp = {
-        init_options = {
-          config = "~/.config/nvim/lua/plugins/.typos.toml",
+if vim.g.vscode then
+  return { "neovim/nvim-lspconfig", enabled = false }
+else
+  return {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        typos_lsp = {
+          init_options = {
+            config = "~/.config/nvim/lua/plugins/.typos.toml",
+          },
         },
-      },
-      ruby_lsp = {
-        mason = false,
-        cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
-      },
-      rubocop = {
-        mason = false,
-        cmd = { "bundle", "exec", "rubocop", "--lsp" },
-      },
-      solargraph = {
-        cmd = { "solargraph", "stdio" },
-        settings = {
-          solargraph = {
-            diagnostics = true,
-            useBundler = false,
-            formatting = true,
-            reporters = { "require_not_found" },
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+        },
+        rubocop = {
+          mason = false,
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        },
+        solargraph = {
+          cmd = { "solargraph", "stdio" },
+          settings = {
+            solargraph = {
+              diagnostics = true,
+              useBundler = false,
+              formatting = true,
+              reporters = { "require_not_found" },
+            },
           },
         },
       },
     },
-  },
-}
+  }
+end
