a914e3fa2d368c66690531add74d6aa27b5f028f
charo <masafumi.matsunaga@smarthr.co.jp>
2026-02-12 18:43:10 +0900

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,15 +5,15 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- color scheme
-config.color_scheme = "Everforest Dark (Gogh)"
+config.color_scheme = "Tokyo Night"
 
 -- opacity
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 20
 
 -- font
 config.font = wezterm.font("HackGen Console NF", { weight = "Regular" })
-config.font_size = 18.0
+config.font_size = 20.0
 
 -- key bindings
 config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 2000 }
