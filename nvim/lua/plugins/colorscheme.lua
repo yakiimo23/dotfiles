ab3a6bd63230ea2b99ca807cfa884e2f92ad93e4
charo <masafumi.matsunaga@smarthr.co.jp>
2026-02-12 18:43:35 +0900

    update: nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,30 @@
 return {
+  -- {
+  --   "neanias/everforest-nvim",
+  --   lazy = true,
+  --   priority = 1000,
+  --   config = function()
+  --     require("everforest").setup({
+  --       transparent_background_level = 0.9,
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "everforest",
+  --   },
+  -- },
   {
-    "neanias/everforest-nvim",
+    "folke/tokyonight.nvim",
     lazy = true,
-    priority = 1000,
-    config = function()
-      require("everforest").setup({
-        transparent_background_level = 0.9,
-      })
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      style = "moon",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
